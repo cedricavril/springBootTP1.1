7798d3a857a1a27347619d57689c0a90388dfa87
cedric avril <cedric_avril_pro@hotmail.fr>
2020-01-14 21:47:36 +0100

    chapitre 4.3

--- src/main/java/com/example/springboot/SimpleController.java
+++ src/main/java/com/example/springboot/SimpleController.java
@@ -1,8 +1,10 @@
 package com.example.springboot;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +16,35 @@ public class SimpleController {
 	String appName;
 	@Value("${spring.application.dateTitle}")
 	String dateTitle;
+	
+	 private static final Logger LOGGER = LoggerFactory.getLogger(SimpleController.class);	
+	
 	@GetMapping("/")
 	public String homePage(Model model) {
 		model.addAttribute("appName", appName);
 	return "home";
 	}
+
+	
 	@GetMapping("/datepage")
 	public String datePage(Model model) {
-		model.addAttribute("dateTitle", dateTitle);
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		String dateStr = simpleDateFormat.format(new Date()); 
+		int 	intMonth = LocalDateTime.now().getMonthValue(),
+				intDay = LocalDateTime.now().getDayOfMonth();
+		String	season = "hiver";
+		int currentAnnualTimeStamp = intDay * intMonth,
+			springTimeline = 21 * 3,
+			summerTimeline = 21 * 6,
+			fallTimeline = 21 * 9,
+			winterTimeline = 21 * 12;
 
-		model.addAttribute("localDateTime", dateStr);
+		if (currentAnnualTimeStamp >= springTimeline) season = "printemps";
+		else if (currentAnnualTimeStamp >= summerTimeline) season = "été";
+		else if (currentAnnualTimeStamp >= fallTimeline) season = "automne";
+		else if (currentAnnualTimeStamp >= winterTimeline) season = "hiver";
+
+		model.addAttribute("dateTitle", dateTitle);
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		model.addAttribute("season", season);
 	return "datepage";
 	}
 }
